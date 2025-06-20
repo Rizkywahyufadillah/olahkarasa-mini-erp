256b68d3b1bedf702cca416b94f52222539c613f
Rizky Wahyu <rizkywahyufadillah3@gmail.com>
2025-06-20 14:12:35 +0700

    init

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::view('/', 'index');
+Route::view('/buttons', 'buttons');
+Route::view('/cards', 'cards');
+Route::view('/charts', 'charts');
+Route::view('/forms', 'forms');
+Route::view('/modals', 'modals');
+Route::view('/tables', 'tables');
